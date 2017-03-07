229fbd7db318c4f990e41b201ed08f4125e581e7
Brooke Fox <brooke.fox@c02mn8jhfd57.corp.aofl.com>
2017-03-06 16:28:41 -0800

    remove code from javascript/food-chain/food-chain.js

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -1,18 +1,5 @@
 var FoodChain = function() {};
 
-FoodChain.prototype.matches = function(possibilities) {
-  if (arguments.length > 1 || typeof possibilities == "string") {
-    possibilities = Object.values(arguments);
-  }
-  var letters = this.string.toLowerCase().match(/[a-z]/g).sort(),
-      matches = [];
-  for (var i = 0; i < possibilities.length; i++) {
-    if ((letters.equals(possibilities[i].toLowerCase().match(/[a-z]/g).sort())) &&
-        (this.string.toLowerCase() != possibilities[i].toLowerCase())) {
-      matches.push(possibilities[i]);
-    }
-  }
-  return matches;
-};
+FoodChain.prototype.verse = function(num) {};
 
 module.exports = FoodChain;

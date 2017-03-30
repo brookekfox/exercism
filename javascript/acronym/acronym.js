b1aa9ece589cf8a0f559f9e2b90eb53c37815b07
Brooke Fox <brooke.fox@c02mn8jhfd57.corp.aofl.com>
2017-03-30 11:54:27 -0700

    acronym, scrabble-score

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -1,7 +1,20 @@
 var Acronyms = {
   parse: function(value) {
-    console.log(value);
+    var capitals = value.match(/[A-Z]/g);
+    var firstLetters = value.replace('-', ' ').split(' ').map(function(w) {
+      w = w.toUpperCase();
+      return w[0];
+    });
+    if ((firstLetters.length > capitals.length) || value.match(/[:]/g)) {
+      return firstLetters.join('');
+    } else {
+      return capitals.join('');
+    }
   }
 };
+
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
 
 module.exports = Acronyms;

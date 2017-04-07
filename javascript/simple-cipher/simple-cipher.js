8e2363dacf725bac22c96d1b7178ed19c0c317a5
Brooke Fox <brooke.fox@c02mn8jhfd57.corp.aofl.com>
2017-04-06 17:48:48 -0700

    simple cipher

---
+++ javascript/simple-cipher/simple-cipher.js
@@ -0,0 +1,9 @@
+var SimpleCipher = function() {};
+
+SimpleCipher.prototype.key = function() {}
+
+SimpleCipher.prototype.encode = function() {}
+
+SimpleCipher.prototype.decode = function() {}
+
+module.exports = SimpleCipher;
